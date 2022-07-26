59df0845c18ba7c3f8acadba08b77f5fc610e538
Karim <Karim.krayem@gmail.com>
2022-07-26 14:31:23 +0200

    added "done" btn

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,9 @@ let input = document.querySelector(".todo-input")
 let add = document.querySelector(".add")
 let list = document.querySelector(".list");
 let newInput = document.querySelector(".newInput");
-let NewInputValue = document.querySelector(".newInputValue");
+let newInputValue = document.querySelector(".newInputValue");
+let comfirmer = document.querySelector(".comfirmer");
+let annuler = document.querySelector(".annuler")
 
 
 
@@ -12,33 +14,40 @@ let NewInputValue = document.querySelector(".newInputValue");
 
 
 add.addEventListener("click", function(){
-    let listItem = document.createElement("div")
-    listItem.className = "listItem"
-    list.appendChild(listItem)
-    let toDo = document.createElement("input");
-    toDo.disabled = "true"
-
-    toDo.value += input.value
-    listItem.appendChild(toDo)
-    let supprimer = document.createElement("span")
-    supprimer.textContent = "x"
-    listItem.addEventListener("dblclick", function(){
-        toDo.replaceWith(newInput)
-        newInput.className = "newInput"
-        
-        console.log("tii");
-    
-    })
-    listItem.appendChild(supprimer)
-
-
-
-   
-
     if (input.value !== "") {
+        let listItem = document.createElement("div")
+        listItem.className = "listItem"
+        list.appendChild(listItem)
+
+        let toDo = document.createElement("input");
+        toDo.disabled = "true"
+        toDo.value += input.value
+        listItem.appendChild(toDo)
+
+        let supprimer = document.createElement("span")
+        supprimer.textContent = "x"
+        listItem.appendChild(supprimer)
+
+
+        let valider = document.createElement("span");
+        valider.textContent = "valider"
+        listItem.appendChild(valider)
+
+
+        valider.addEventListener("click", function(){
+            if (toDo.className == "done"){
+                toDo.removeAttribute('class')
+            }else{
+                toDo.classList = "done"
+
+            }
+        })
         supprimer.addEventListener("click", function(e){
             console.log(e);
             list.removeChild(e.path[1])
+
+
+
             
             
         }) 
@@ -49,14 +58,15 @@ add.addEventListener("click", function(){
 
     }
 
-    
-
-
-
-
 })
 
 
+annuler.addEventListener("click", function(){
+    console.log("yoo");
+})
+
+
+// dbl(){}
 
 
     // supprimer.addEventListener()
